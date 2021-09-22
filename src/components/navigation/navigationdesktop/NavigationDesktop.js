157ab5ce44d96fd3ddfff8e39ebddac4d107fb1d
SanjinAjanic <74593957+SanjinAjanic@users.noreply.github.com>
2021-09-22 19:18:11 +0200

    lagt in css i min nav

--- src/components/navigation/navigationdesktop/NavigationDesktop.js
+++ src/components/navigation/navigationdesktop/NavigationDesktop.js
@@ -2,24 +2,37 @@ import React,{useContext, useState} from 'react'
 import { useHistory } from "react-router";
 import RoutingPath from '../../../routes/RoutingPath';
 import { UserContext } from '../../../shared/provider/UserProvider';
+import "./NavigationDesktop.css";
 
 export const NavigationDesktop = () => {
     const history = useHistory();
     const [authenticatedUser, setAuthenticatedUser] = useContext(UserContext);
     const showLogin = () => {
 
-        if(authenticatedUser !== null) return <span>{authenticatedUser}</span>
+        if(authenticatedUser !== null) 
+        return( <span className="navbar_login_desktop">{authenticatedUser}</span>
+        );
 
         return (
-            <button onClick = {() => history.push(RoutingPath.signInView,"Nu kan du logga in")}> Sign in</button>
+            <button className="navbar_login_desktop" 
+            onClick = {() => history.push(RoutingPath.signInView,"Nu kan du logga in")}> Sign in</button>
         );
 
     };
+
     return (
-        <nav>
+        <nav className="navbar_desktop">
+            <span className="navbar_logo_desktop"></span>
+            <ul className="navbar_list_deskop">
+                <li>
             <button onClick = {() => history.push(RoutingPath.homeView,"Nu är du hemma!") } > Home</button>
+            </li>
+            <li>
             <button onClick = {() => history.push(RoutingPath.storeView,"Nu är du i butiken") } > Store</button>
+            </li>
+            </ul>
             {showLogin()}
         </nav>
+    
     );
 };
