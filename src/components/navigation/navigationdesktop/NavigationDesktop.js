6fae50494701e8e14486f41ebcf03ab4ff0a9202
SanjinAjanic <74593957+SanjinAjanic@users.noreply.github.com>
2021-09-22 18:16:18 +0200

    skapat login funktion och ändrat så att man byter sida när man väl loggat in

--- src/components/navigation/navigationdesktop/NavigationDesktop.js
+++ src/components/navigation/navigationdesktop/NavigationDesktop.js
@@ -1,14 +1,25 @@
-import React from 'react'
+import React,{useContext, useState} from 'react'
 import { useHistory } from "react-router";
 import RoutingPath from '../../../routes/RoutingPath';
+import { UserContext } from '../../../shared/provider/UserProvider';
 
 export const NavigationDesktop = () => {
     const history = useHistory();
+    const [authenticatedUser, setAuthenticatedUser] = useContext(UserContext);
+    const showLogin = () => {
+
+        if(authenticatedUser !== null) return <span>{authenticatedUser}</span>
+
+        return (
+            <button onClick = {() => history.push(RoutingPath.signInView,"Nu kan du logga in")}> Sign in</button>
+        );
+
+    };
     return (
         <nav>
             <button onClick = {() => history.push(RoutingPath.homeView,"Nu är du hemma!") } > Home</button>
             <button onClick = {() => history.push(RoutingPath.storeView,"Nu är du i butiken") } > Store</button>
-            <button onClick = {() => history.push(RoutingPath.signInView,"Nu kan du logga in")}> Sign in</button>
+            {showLogin()}
         </nav>
-    )
-}
+    );
+};
--- src/views/singinview/SignInView.js
+++ src/views/singinview/SignInView.js
@@ -1,18 +1,27 @@
-
 import { useState, useContext } from 'react'
 import { UserContext } from "../../shared/provider/UserProvider";
+import { useHistory } from 'react-router';
+import RoutingPath from '../../routes/RoutingPath';
+
 
 export const SignInView = () => {
     const [username, setUsername] = useState()
     const [password, setPassword] = useState();
     const [authenticatedUser, setAuthenticatedUser] = useContext(UserContext);
+    const history = useHistory();
+
+    const logIn = () =>{
+        setAuthenticatedUser(username);
+        history.push(RoutingPath.homeView);
+    };
+
     return (
         <div>
             <h1>You are in the SignInView</h1>
-            <h2> Username: {authenticatedUser} </h2>
+            <h2> {username} </h2>
             <input
              placeholder="Enter username"  
-             onChange = {(event) => setAuthenticatedUser(event.target.value)}
+             onChange = {(event) => setUsername(event.target.value)}
             ></input>{" "} 
             <br/>  
             <input type="password" 
@@ -20,9 +29,9 @@ export const SignInView = () => {
             onChange = {(event) => setPassword(event.target.value)}
             ></input>{" "} 
             <br/>
-            <button>Log in</button>
+            <button onClick={logIn}>Log in</button>
            
-          
+    
         </div>
     )
 }
